392f0923a7f00270375886009bfa4ad4aabe6766
José Miguel <urux11@gmail.com>
2024-10-27 06:55:01 -0500

    Figured out their time system. More work to do with the prefix info requests

--- infocaudalesbot.js
+++ infocaudalesbot.js
@@ -21,17 +21,31 @@ async function postTweet() {
 
 //get information
 function getInfo(id){
+    var fecha = new Date(); //UTC date, 5 hours ahead
+    var anio = fecha.getUTCFullYear().toString();
+    var intmes = (fecha.getUTCMonth()+1).toString();
+    var mes = (intmes.length == 1) ? "0" + intmes : intmes;
+    var intdia = (fecha.getUTCDate()).toString();
+    var dia = (intdia.length == 1) ? "0" + intdia : intdia;
+    
+    //params
+    var p_fechaInicio = anio + "-" + mes + "-" + dia + "T06:00:00.000Z";
+    var p_fechaFin = new Date(p_fechaInicio);
+    p_fechaFin.setHours(p_fechaFin.getHours() + 23);
+    var p_fecha = dia + "/" + mes + "/" + anio + " 01:00:00";
+
+
     axios.get('https://generacioncsr.celec.gob.ec:8443/ords/csr/sardomcsr/pointValues', {
         params: {
             mrid: id,
-            fechaInicio: '2024-10-26T06:00:00.000Z',
-            fechaFin: '2024-10-27T05:00:00.000Z',
-            fecha: '26/10/2024 01:00:00'
+            fechaInicio: p_fechaInicio,
+            fechaFin: p_fechaFin,
+            fecha: p_fecha
         }
     })
     .then(response => {
         console.log(response.data.items);
-        return response.data;
+        return response.data.items;
 
     })
     .catch(error => {
@@ -40,9 +54,6 @@ function getInfo(id){
     });
 }
 
-const urlHandles = {
-
-}
 
 var mazar = {
     nombre: "Mazar-Paute",
@@ -52,7 +63,8 @@ var mazar = {
     prefix: "maz",
     turbinas_id: 30503,
     caudal_id: 30538,
-    cota_id: 30031
+    cota_id: 30031,
+    paute: true
 };
 
 var molino = {
@@ -63,7 +75,8 @@ var molino = {
     prefix: "mol",
     turbinas_id: 44822,
     caudal_id: 24811,
-    cota_id: 24019
+    cota_id: 24019,
+    paute: true
 };
 
 var sopladora = {
@@ -74,21 +87,21 @@ var sopladora = {
     prefix: "sop",
     turbinas_id: 90503,
     caudal_id: 90537,
-    cota_id: 90919
+    cota_id: 90919,
+    paute: true
 };
 
 var minas_san_francisco = {
-    nombre: "Minas San Francisco",
+    nombre: "MinasSanFrancisco",
     cotaMin: 783,
     cotaMax: 792,
     energiaMax: 270,
     prefix: "msf",
     turbinas_id: 650503,
     caudal_id: 650538,
-    cota_id: 650919
+    cota_id: 650919,
+    paute: false
 };
-
-var hidroelectricas = [mazar, sopladora, molino, minas_san_francisco];
 
 var celec_sur = {
     nombre: "CELEC EP SUR",
@@ -96,21 +109,29 @@ var celec_sur = {
     caudal_id: 24812 
 }
 
-getInfo(mazar.caudal_id);
+var hidroelectricas = [mazar, sopladora, molino, minas_san_francisco];
 
 //get those values
-console.log(hidroID.mazarEnergia);
-console.log(hidroID["mazarEnergia"]);
 var fecha = new Date();
 console.log(new Date(fecha.getTime() - 5 * 60 * 60 * 1000)); //works to do GMT-5
+console.log(fecha.getUTCDay());
 
 for(var central of hidroelectricas){
     console.log("Hidroeléctrica "+ central.nombre);
     for(var dato in central){
-        console.log(dato,":", central[dato]);
+        console.log(dato+":", central[dato]);
     }
     console.log("\n");
 }
+
+function postearInfo(){
+
+
+
+}
+
+
+
 /*
 note that
 mazEnerDia
@@ -120,7 +141,7 @@ msfEnerDia
 csrEnerDia
 dont use ids, instead, they use their own url handles and dont use the pointValues param
 
-i guess i'll have to alter Date and Time so it is synced with GMT-5 (ecuadorian time), because Verpex's servers are in london
+i guess i'll have to alter Date and Time so it is synced with GMT-5 (ecuadorian time), because Verpex's servers are in london (or in this case US east)
 
 
 all this was taken around 15h37
@@ -141,4 +162,12 @@ https://generacioncsr.celec.gob.ec:8443/ords/csr/sardomcsr/pointValues?mrid=2481
 
 
 when requesting information, oldest timestamps are last, that means that 00h00 is items[items.length], and 24h00 is items[0]
+
+every single request has:
+fechaInicio: year-month-dayT06:00:00.000Z
+fechaFin year-month-{day+1}-T05:00:00.000Z
+fecha: day/month/year%2001:00:00
+but this starts from 01h00 to 24h00, i dont want that but they only support that
+
+the response value according to actual time
 */
