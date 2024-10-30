34fccc0e8ad99a36509023133b38e567fe470ee5
José Miguel <urux11@gmail.com>
2024-10-29 23:42:41 -0500

    Connection to the server

--- infocaudalesbot.js
+++ infocaudalesbot.js
@@ -1,3 +1,13 @@
+var http = require('http');
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var message = 'It works!\n',
+        version = 'NodeJS ' + process.versions.node + '\n',
+        response = [message, version].join('\n');
+    res.end(response);
+});
+server.listen();
+
 const axios = require('axios');
 const { TwitterApi } = require('twitter-api-v2');
 const CronJob = require('cron').CronJob;
