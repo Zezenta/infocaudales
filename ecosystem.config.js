9f679833c2fa4a0fb12c2fead1923c6598b165ca
José Miguel <urux11@gmail.com>
2026-06-28 23:18:54 -0500

    fork mode for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,15 @@ module.exports = {
     {
       name: 'infocaudales',
       script: 'dist/src/index.js',
+      exec_mode: 'fork',
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '500M',
       env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
         NODE_ENV: 'production',
       },
     },
