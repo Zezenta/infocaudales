ec69f0bf52097f94d4c2f9de7962fcf062118857
José Miguel <urux11@gmail.com>
2026-06-28 21:35:28 -0500

    github actions

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'infocaudales',
+      script: 'dist/src/index.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '500M',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
